88a5c5b8bd243e2d20fb72c91067a1fbcb1ae446
Moritz Lang <hi@slashmo.codes>
2023-09-09 03:07:04 +0200

    Add MigrationRepository protocol

---
+++ Sources/Chinchilla/Repository/MigrationRepository.swift
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Chinchilla open source project
+//
+// Copyright (c) 2023 Moritz Lang and the Chinchilla project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+public protocol MigrationRepository {
+    func migrations() async throws -> [Migration]
+}
